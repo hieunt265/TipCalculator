30b9943a2571b9685d845bb9d73d856212c97ce2
HieuNT <hieunt@HieuNTs-MacBook-Pro.local>
2017-05-28 14:06:50 +0700

    update basic calculator

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,27 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+    }
+    
+    @IBAction func calulatorTip(_ sender: Any) {
+       
+        
+        
+        let tipPercent = [0.18,0.2, 0.25]
+        
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip =   bill * tipPercent[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text =  String(format: "$%.2f" ,(tip))
+        totalLabel.text = String(format: "$%.2f" ,(total))
+
     }
 
 
